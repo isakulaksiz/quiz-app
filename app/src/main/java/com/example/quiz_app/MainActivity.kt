94304ecb41c0f9db22171a28a1b441d06a3a4320
isakulaksiz <isakulaksiz@outlook.com>
2022-01-06 12:16:51 +0300

    [#1]feat: added cardview and disable statusbar

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.quiz_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // main sayfasının yukarısında bildirimleri kaldır
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
