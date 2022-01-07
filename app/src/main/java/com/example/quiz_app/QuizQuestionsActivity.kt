f229246f86d959571b98b6fc4d61048c59d5d355
isakulaksiz <isakulaksiz@outlook.com>
2022-01-07 17:05:18 +0300

    [#3]fix: connected qouestions class within ViewBinding

--- app/src/main/java/com/example/quiz_app/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quiz_app/QuizQuestionsActivity.kt
@@ -4,14 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import com.example.quiz_app.databinding.ActivityMainBinding
+import com.example.quiz_app.databinding.ActivityQuizQuestionsBinding
+import com.example.quiz_app.model.Questions
 
 class QuizQuestionsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityQuizQuestionsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz_questions)
+        binding = ActivityQuizQuestionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         val questionsList   = Constants.getQuestions()
         Log.i("Soru adeti ","${questionsList.size}")
+
+        val currentPosition = 1
+        val question: Questions? = questionsList[currentPosition -1] // arrayList'in 0. indis değerinde ki  değeri
+
+        binding.progressBar.progress = currentPosition
+        binding.tvProgress.text = "$currentPosition" + "/" + binding.progressBar.max
+
+        binding.tvQuestionId.text = question!!.question
+        binding.ivImage.setImageResource(question.image)
+        binding.tvOptionOne.text = question.optionOne
+        binding.tvOptionTwo.text = question.optionTwo
+        binding.tvOptionThree.text = question.optionThree
+        binding.tvOptionFour.text = question.optionFour
     }
 }
