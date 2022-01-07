14ea3342ddb54205acb6baed7c3da704b823d625
isakulaksiz <isakulaksiz@outlook.com>
2022-01-07 16:05:46 +0300

    fixed typo

--- app/src/main/java/com/example/quiz_app/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quiz_app/QuizQuestionsActivity.kt
@@ -3,12 +3,14 @@ package com.example.quiz_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         val questionsList   = Constants.getQuestions()
         Log.i("Soru adeti ","${questionsList.size}")
     }
