511a05a156e337dc6850cfd506dbf4cd9e710446
İsa Kulaksız <isakulaksiz@outlook.com>
2022-02-18 17:46:25 +0300

    fix typo

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var languageSettings: LanguageSettings
     var language: String = ""
+    var test: String = "quorion"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initLanguage()
